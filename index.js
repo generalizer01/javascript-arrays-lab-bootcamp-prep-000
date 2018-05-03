0094125ba15516334abd145f263e72acf145113b
guymarsden01@gmail.com <guymarsden01@gmail.com>
2018-05-03 13:02:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,20 @@ var kittens = ["Milo","Otis","Garfield"]
 
 function destructivelyAppendKitten(name) {
   kittens.push(name);
+  return kittens
 }
 
 function destructivelyPrependKitten(name) {
-  kittens.
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.unshift()
+  return kittens
 }
